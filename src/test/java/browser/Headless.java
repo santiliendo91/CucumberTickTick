2042aa654a98c07621306b97d3352a444af81a45
Santiago Liendo <santi.liendo91@gmail.com>
2022-12-20 13:02:28 -0400

    Added Tick Tick cucumber

---
+++ src/test/java/browser/Headless.java
@@ -0,0 +1,22 @@
+package browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Headless implements IBrowser{
+    @Override
+    public WebDriver create() {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/driver/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        return driver;
+    }
+}
